497456db807872ceb16f0f3c4a3adfe4a05d266f
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-03-13 19:13:57 +0900

    Example シーン追加

---
+++ Examples/TRSSender.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace AffineDecomposition.Examples {
+
+    [ExecuteAlways]
+    public class TRSSender : MonoBehaviour {
+
+        public Events events = new Events();
+
+        void Update() {
+            var m = (float4x4)transform.localToWorldMatrix;
+            var trs = new float3x4(m.c0.xyz, m.c1.xyz, m.c2.xyz, m.c3.xyz).DecomposeToTRS();
+
+            var pos = trs.translate;
+            var rot = trs.rotate;
+            var scl = new float3(trs.stretch[0][0], trs.stretch[1][1], trs.stretch[2][2]);
+
+            events.TranslateOnChanged.Invoke(pos);
+            events.RotateOnChanged.Invoke(rot);
+            events.ScaleOnChanged.Invoke(scl);
+        }
+
+        [System.Serializable]
+        public class Events {
+            public Vector3Event TranslateOnChanged = new Vector3Event();
+            public QuaternionEvent RotateOnChanged = new QuaternionEvent();
+            public Vector3Event ScaleOnChanged = new Vector3Event();
+
+            [System.Serializable]
+            public class Vector3Event : UnityEvent<Vector3> { }
+            [System.Serializable]
+            public class QuaternionEvent : UnityEvent<Quaternion> { }
+        }
+    }
+}
---
+++ Scripts/Oscillator.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AffineDecomposition.Examples {
+
+    public class Oscillator : MonoBehaviour {
+        public const float TWO_PI = 2f * Mathf.PI;
+
+        public Tuner tuner = new Tuner();
+
+        private void Update() {
+            var t = Time.realtimeSinceStartup;
+
+            transform.localPosition = Vector3.Lerp(
+                tuner.translateFrom, 
+                tuner.translateTo, 
+                Oscilate(tuner.translateOsc * t));
+
+            transform.localRotation = Quaternion.Euler(Vector3.Lerp(
+                tuner.rotateFrom, 
+                tuner.rotateTo, 
+                Oscilate(tuner.rotateOsc * t)));
+
+            transform.localScale = Vector3.Lerp(
+                tuner.scaleFrom, 
+                tuner.scaleTo, 
+                Oscilate(t * tuner.scaleOsc));
+        }
+
+        public static float Oscilate(float t) {
+            return Mathf.Clamp01(0.5f * (Mathf.Cos(t * TWO_PI) + 1f));
+        }
+
+        [System.Serializable]
+        public class Tuner {
+            public Vector3 translateFrom = Vector3.zero;
+            public Vector3 translateTo = Vector3.zero;
+            [Range(0f, 1f)]
+            public float translateOsc = 0f;
+
+            public Vector3 rotateFrom = Vector3.zero;
+            public Vector3 rotateTo = Vector3.zero;
+            [Range(0f, 1f)]
+            public float rotateOsc = 0f;
+
+            public Vector3 scaleFrom = Vector3.one;
+            public Vector3 scaleTo = Vector3.one;
+            [Range(0f, 10f)]
+            public float scaleOsc = 0f;
+        }
+
+    }
+}
